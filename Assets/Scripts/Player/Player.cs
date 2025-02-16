b48b85f529ac12e4a759e4d8c4831bdc7242eb31
victorlevy100 <44725310+victorlevy100@users.noreply.github.com>
2025-02-16 08:33:44 -0400

    Adding the jump to the player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,27 +5,46 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody2D;
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f,0);
 
     public float speed;
+
+    public float forceJump = 2f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Space))
+
+        HandleMovement();
+        HandleJump();
+    }
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("Jump");
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            // myRigidbody2D.MovePosition(myRigidbody2D.position - velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(-speed, myRigidbody2D.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            // myRigidbody2D.MovePosition(myRigidbody2D.position + velocity * Time.deltaTime);
             myRigidbody2D.velocity = new Vector2(speed, myRigidbody2D.velocity.y);
         }
 
+        if(myRigidbody2D.velocity.x < 0)
+        {
+            myRigidbody2D.velocity += friction;
+        }
+        else if (myRigidbody2D.velocity.x > 0)
+        {
+            myRigidbody2D.velocity -= friction;
+        }
+    }
+    void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = Vector2.up * forceJump;
+        }
     }
 }
+    
+
